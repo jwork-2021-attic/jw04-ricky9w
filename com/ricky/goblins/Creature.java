95a6d7d2cb1fa62da56489ebe849f2908aeb400e
Richard Wang <ricky9wang@gmail.com>
2021-10-07 23:54:17 +0800

    refactor project

---
+++ com/ricky/goblins/Creature.java
@@ -0,0 +1,16 @@
+package com.ricky.goblins;
+
+import java.awt.Color;
+
+public class Creature extends Thing {
+
+    Creature(Color color, char glyph, World world) {
+        super(color, glyph, world);
+    }
+
+    public void moveTo(int xPos, int yPos) {
+        this.world.put(this, xPos, yPos);
+    }
+
+    
+}
---
+++ com/ricky/goblins/Goblin.java
@@ -0,0 +1,5 @@
+package com.ricky.goblins;
+
+public class Goblin {
+    
+}
